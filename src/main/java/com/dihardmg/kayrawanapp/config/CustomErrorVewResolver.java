e1cb9f664b324b87250c6331afc8f3715d7b7a20
Didik Hariyanto <com.didikhariyanto@gmail.com>
2018-01-04 02:43:49 +0700

    menambahkan @validate.

--- src/main/java/com/dihardmg/kayrawanapp/config/CustomErrorVewResolver.java
+++ src/main/java/com/dihardmg/kayrawanapp/config/CustomErrorVewResolver.java
@@ -3,9 +3,13 @@ package com.dihardmg.kayrawanapp.config;
 import org.springframework.boot.autoconfigure.web.ErrorViewResolver;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
 import java.util.Map;
 
 /**
@@ -34,6 +38,20 @@ public class CustomErrorVewResolver implements ErrorViewResolver {
 
         HttpStatus httpStatus = HttpStatus.valueOf(statusCode);
         return httpStatus.getReasonPhrase();
+    }
+
+    
+    @ExceptionHandler(value = {ConstraintViolationException.class})
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    public String handleValidationFailure(ConstraintViolationException ex) {
+
+        StringBuilder messages = new StringBuilder();
+
+        for (ConstraintViolation<?> violation : ex.getConstraintViolations()) {
+            messages.append(violation.getMessage() + "\n");
+        }
+
+        return messages.toString();
     }
 }
 
